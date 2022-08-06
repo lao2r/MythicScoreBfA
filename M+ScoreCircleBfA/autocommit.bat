33a3e4bc334f2120e7cba1fba60c6207aa8d4bb4
Andrey Labazin <andreylabazin@gmail.com>
2022-08-06 17:06:38 +0300

    123

---
+++ M+ScoreCircleBfA/autocommit.bat
@@ -0,0 +1,6 @@
+git checkout main
+git rm --cached C:/mythic-score-app/M+ScoreCircleBfA/ -r
+git add -u C:/mythic-score-app/M+ScoreCircleBfA/characters_db.lua
+git add -u C:/mythic-score-app/M+ScoreCircleBfA/characters_mapping.lua
+git commit -m "Daily update"
+git push https://github.com/lao2r/MythicScoreBfA main
