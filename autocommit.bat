a9d62061d3fe2feb6174a028faffe8ff38149a49
Andrew Souls <83645806+lao2r@users.noreply.github.com>
2023-11-05 16:16:16 +0300

    Delete autocommit.bat

--- autocommit.bat
+++
@@ -1,6 +0,0 @@
-git checkout -b update
-git checkout update
-git remote add origin https://github.com/lao2r/MythicScoreBfA.git
-git add ./
-git commit -m "Daily udate"
-git push -u -f origin update:main
